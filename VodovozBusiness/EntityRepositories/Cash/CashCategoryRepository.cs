6c80875614030d89f0748987868c290aab22280e
Vadim <enzogord@gmail.com>
2020-08-17 12:34:00 +0300

    Замена статического класса репозитория статей кассы, на обычный

---
+++ VodovozBusiness/EntityRepositories/Cash/CashCategoryRepository.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using NHibernate.Criterion;
+using QS.DomainModel.UoW;
+using Vodovoz.Domain.Cash;
+using Vodovoz.Services;
+
+namespace Vodovoz.EntityRepositories.Cash
+{
+	public class CashCategoryRepository : ICashCategoryRepository
+	{
+		private readonly ICashCategoryParametersProvider cashCategoryParametersProvider;
+
+		public CashCategoryRepository(ICashCategoryParametersProvider cashCategoryParametersProvider)
+		{
+			this.cashCategoryParametersProvider = cashCategoryParametersProvider ?? throw new ArgumentNullException(nameof(cashCategoryParametersProvider));
+		}
+
+		public IncomeCategory DefaultIncomeCategory(IUnitOfWork uow)
+		{
+			return uow.GetById<IncomeCategory>(cashCategoryParametersProvider.DefaultIncomeCategory);
+		}
+
+		public ExpenseCategory EmployeeSalaryExpenseCategory(IUnitOfWork uow)
+		{
+			return uow.GetById<ExpenseCategory>(cashCategoryParametersProvider.EmployeeSalaryExpenseCategory);
+		}
+
+		public IList<ExpenseCategory> ExpenseCategories(IUnitOfWork uow)
+		{
+			return uow.Session.QueryOver<ExpenseCategory>().OrderBy(ec => ec.Name).Asc().List();
+		}
+
+		public QueryOver<ExpenseCategory> ExpenseCategoriesQuery()
+		{
+			return QueryOver.Of<ExpenseCategory>().OrderBy(ec => ec.Name).Asc();
+		}
+
+		public IList<ExpenseCategory> ExpenseSelfDeliveryCategories(IUnitOfWork uow)
+		{
+			return uow.Session.QueryOver<ExpenseCategory>()
+				.Where(x => x.ExpenseDocumentType == ExpenseInvoiceDocumentType.ExpenseInvoiceSelfDelivery)
+				.OrderBy(ec => ec.Name).Asc()
+				.List();
+		}
+
+		public ExpenseCategory FuelDocumentExpenseCategory(IUnitOfWork uow)
+		{
+			return uow.GetById<ExpenseCategory>(cashCategoryParametersProvider.FuelDocumentExpenseCategory);
+		}
+
+		public IList<IncomeCategory> IncomeCategories(IUnitOfWork uow)
+		{
+			return uow.Session.QueryOver<IncomeCategory>().OrderBy(ic => ic.Name).Asc().List();
+		}
+
+		public QueryOver<IncomeCategory> IncomeCategoriesQuery()
+		{
+			return QueryOver.Of<IncomeCategory>().OrderBy(ic => ic.Name).Asc();
+		}
+
+		public ExpenseCategory RouteListClosingExpenseCategory(IUnitOfWork uow)
+		{
+			return uow.GetById<ExpenseCategory>(cashCategoryParametersProvider.RouteListClosingExpenseCategory);
+		}
+
+		public IncomeCategory RouteListClosingIncomeCategory(IUnitOfWork uow)
+		{
+			return uow.GetById<IncomeCategory>(cashCategoryParametersProvider.RouteListClosingIncomeCategory);
+		}
+
+		public IList<IncomeCategory> SelfDeliveryIncomeCategories(IUnitOfWork uow)
+		{
+			return uow.Session.QueryOver<IncomeCategory>()
+				.Where(x => x.IncomeDocumentType == IncomeInvoiceDocumentType.IncomeInvoiceSelfDelivery)
+				.OrderBy(ic => ic.Name).Asc()
+				.List();
+		}
+	}
+}
--- VodovozBusiness/Repositories/Cash/CategoryRepository.cs
+++ VodovozBusiness/Repositories/Cash/CategoryRepository.cs
@@ -4,9 +4,12 @@ using NHibernate.Criterion;
 using QS.DomainModel.UoW;
 using Vodovoz.Domain.Cash;
 using Vodovoz.Parameters;
+using Vodovoz.EntityRepositories.Cash;
+using Vodovoz.Domain.Service.BaseParametersServices;
 
 namespace Vodovoz.Repository.Cash
 {
+	[Obsolete("Устарел. Используйте CashCategoryRepository")]
 	public class CategoryRepository
 	{
 		const string defaultIncomeCategory 			 = "default_income_category";
@@ -15,87 +18,54 @@ namespace Vodovoz.Repository.Cash
 		const string fuelDocumentExpenseCategory 	 = "fuel_expense";
 		const string employeeSalaryExpenseCategory   = "employee_salary"; 		// Параметр базы для статьи расхода для авансов.
 
+		private static ICashCategoryRepository GetCategoryRepository()
+		{
+			return new CashCategoryRepository(new CashCategoryParametersProvider(new ParametersProvider()));
+		}
+
 		public static IList<IncomeCategory> IncomeCategories (IUnitOfWork uow)
 		{
-			return uow.Session.QueryOver<IncomeCategory> ().OrderBy (ic => ic.Name).Asc ().List ();
+			return GetCategoryRepository().IncomeCategories(uow);
 		}
 
 		public static IList<IncomeCategory> SelfDeliveryIncomeCategories(IUnitOfWork uow)
 		{
-			return uow.Session.QueryOver<IncomeCategory>()
-				.Where(x => x.IncomeDocumentType == IncomeInvoiceDocumentType.IncomeInvoiceSelfDelivery)
-				.OrderBy(ic => ic.Name).Asc()
-				.List();
+			return GetCategoryRepository().SelfDeliveryIncomeCategories(uow);
 		}
 
 		public static IList<ExpenseCategory> ExpenseCategories (IUnitOfWork uow)
 		{
-			return uow.Session.QueryOver<ExpenseCategory> ().OrderBy (ec => ec.Name).Asc ().List ();
+			return GetCategoryRepository().ExpenseCategories(uow);
 		}
 
 		public static IList<ExpenseCategory> ExpenseSelfDeliveryCategories(IUnitOfWork uow)
 		{
-			return uow.Session.QueryOver<ExpenseCategory>()
-				.Where(x => x.ExpenseDocumentType == ExpenseInvoiceDocumentType.ExpenseInvoiceSelfDelivery)
-				.OrderBy(ec => ec.Name).Asc()
-				.List();
+			return GetCategoryRepository().ExpenseSelfDeliveryCategories(uow);
 		}
 
 		public static QueryOver<ExpenseCategory> ExpenseCategoriesQuery ()
 		{
-			return QueryOver.Of<ExpenseCategory> ().OrderBy (ec => ec.Name).Asc ();
+			return GetCategoryRepository().ExpenseCategoriesQuery();
 		}
 
 		public static QueryOver<IncomeCategory> IncomeCategoriesQuery ()
 		{
-			return QueryOver.Of<IncomeCategory> ().OrderBy (ic => ic.Name).Asc ();
+			return GetCategoryRepository().IncomeCategoriesQuery();
 		}
 
 		public static IncomeCategory DefaultIncomeCategory (IUnitOfWork uow)
 		{
-			if (ParametersProvider.Instance.ContainsParameter(defaultIncomeCategory)) {
-				int id = -1;
-				id = int.Parse (ParametersProvider.Instance.GetParameterValue(defaultIncomeCategory));
-				if (id == -1)
-					return null;
-				return uow.Session.QueryOver<IncomeCategory> ()
-					.Where (inc => inc.Id == id)
-					.Take (1)
-					.SingleOrDefault ();
-			}
-			return null;
+			return GetCategoryRepository().DefaultIncomeCategory(uow);
 		}
 
 		public static IncomeCategory RouteListClosingIncomeCategory(IUnitOfWork uow)
 		{
-			if (ParametersProvider.Instance.ContainsParameter(routeListClosingIncomeCategory))
-			{
-				int id = -1;
-				id = int.Parse (ParametersProvider.Instance.GetParameterValue(routeListClosingIncomeCategory));
-				if (id == -1)
-					return null;
-				return uow.Session.QueryOver<IncomeCategory> ()
-					.Where (inc => inc.Id == id)
-					.Take (1)
-					.SingleOrDefault ();
-			}
-			return null;
+			return GetCategoryRepository().RouteListClosingIncomeCategory(uow);
 		}
 
 		public static ExpenseCategory RouteListClosingExpenseCategory(IUnitOfWork uow)
 		{
-			if (ParametersProvider.Instance.ContainsParameter(routeListClosingExpenseCategory))
-			{
-				int id = -1;
-				id = int.Parse (ParametersProvider.Instance.GetParameterValue(routeListClosingExpenseCategory));
-				if (id == -1)
-					return null;
-				return uow.Session.QueryOver<ExpenseCategory> ()
-					.Where (inc => inc.Id == id)
-					.Take (1)
-					.SingleOrDefault ();
-			}
-			return null;
+			return GetCategoryRepository().RouteListClosingExpenseCategory(uow);
 		}
 
 		internal static Func<IUnitOfWork, ExpenseCategory> FuelDocumentExpenseCategoryTestGap;
@@ -105,36 +75,12 @@ namespace Vodovoz.Repository.Cash
 				return FuelDocumentExpenseCategoryTestGap(uow);
 			}
 
-			if(ParametersProvider.Instance.ContainsParameter(fuelDocumentExpenseCategory))
-			{
-				int id = -1;
-				id = int.Parse (ParametersProvider.Instance.GetParameterValue(fuelDocumentExpenseCategory));
-				if (id == -1)
-					return null;
-				return uow.Session.QueryOver<ExpenseCategory> ()
-					.Where (fExp => fExp.Id == id)
-					.Take (1)
-					.SingleOrDefault ();
-			}
-			return null;
+			return GetCategoryRepository().FuelDocumentExpenseCategory(uow);
 		}
 
 		public static ExpenseCategory EmployeeSalaryExpenseCategory(IUnitOfWork uow)
 		{
-			if(ParametersProvider.Instance.ContainsParameter(employeeSalaryExpenseCategory)) {
-				int id = -1;
-				id = int.Parse(ParametersProvider.Instance.GetParameterValue(employeeSalaryExpenseCategory));
-				if(id == -1)
-					return null;
-				return uow.Session.QueryOver<ExpenseCategory>()
-					.Where(fExp => fExp.Id == id)
-					.Take(1)
-					.SingleOrDefault();
-			}
-			return uow.Session.QueryOver<ExpenseCategory>()
-					.Where(fExp => fExp.Id == 1)
-					.Take(1)
-					.SingleOrDefault();
+			return GetCategoryRepository().EmployeeSalaryExpenseCategory(uow);
 		}
 	}
 }
