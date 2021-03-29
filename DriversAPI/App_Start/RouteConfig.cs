c93d718cbc199f2d2740ad1beb833595ce849a78
Сергей <efimov90@gmail.com>
2021-03-29 15:44:34 +0300

    ZipLibForLinux

--- DriversAPI/App_Start/RouteConfig.cs
+++ DriversAPI/App_Start/RouteConfig.cs
@@ -11,13 +11,13 @@ namespace DriversAPI
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            //routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { action = "Index", id = UrlParameter.Optional }
-            );
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { action = "Index", id = UrlParameter.Optional }
+            //); 
         }
     }
 }
