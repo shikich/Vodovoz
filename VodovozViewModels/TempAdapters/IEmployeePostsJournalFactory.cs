960c9967f0657df21fda70dc8e86c7bf990c15bd
Andrey Shaklein <as1854@yandex.ru>
2021-07-09 12:16:06 +0300

    2792 Перепись на MVVM диалога сотрудника. Переделал фабрику для журнала должностей

---
+++ VodovozViewModels/TempAdapters/IEmployeePostsJournalFactory.cs
@@ -0,0 +1,9 @@
+﻿using QS.Project.Journal.EntitySelector;
+
+namespace Vodovoz.TempAdapters
+{
+	public interface IEmployeePostsJournalFactory
+	{
+		IEntityAutocompleteSelectorFactory CreateEmployeePostsAutoCompleteSelectorFactory(bool multipleSelect = false);
+	}
+}
