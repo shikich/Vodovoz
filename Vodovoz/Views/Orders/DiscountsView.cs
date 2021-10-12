95ad7fcac56c974b86c8f60d700787fa1ffaa520
MikhailFedosov <dobriymisha@gmail.com>
2021-10-12 13:23:06 +0300

    UI add DiscountsView

---
+++ Vodovoz/Views/Orders/DiscountsView.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Bindings.Collections.Generic;
+using System.Linq;
+using Gamma.GtkWidgets;
+using Gamma.Widgets;
+using Gtk;
+using NLog;
+using QS.DomainModel.UoW;
+using QSWidgetLib;
+using Vodovoz.Domain.Client;
+using Vodovoz.Domain.Contacts;
+using Vodovoz.EntityRepositories;
+using Vodovoz.Parameters;
+namespace Vodovoz.Views.Orders
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class DiscountsView : Gtk.Bin
+	{
+		public DiscountsView()
+		{
+			this.Build();
+
+		}
+	}
+}
